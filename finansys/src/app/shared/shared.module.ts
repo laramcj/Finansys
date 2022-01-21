80b864f6b6270d743cae025885db5f12e6bb8445
Lara Correa <lara.mcj@gmail.com>
2022-01-21 09:16:39 -0300

    adding PageHeaderComponent to Pages

--- finansys/src/app/shared/shared.module.ts
+++ finansys/src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
     BreadCrumbComponent,
+    PageHeaderComponent,
   ],
 })
 export class SharedModule {}
